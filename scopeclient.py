be767473be92e0dd3fc6e27914e37cebb55e79b7
Curtis Chou <47016201+choucurtis987@users.noreply.github.com>
2021-11-16 22:17:39 -0800

    updated to be compatible with scopesim V3

--- scopeclient.py
+++ scopeclient.py
@@ -11,7 +11,9 @@ import urllib.request
 class grafit(Frame):
     def plotit(self):
         start_time = time.time()
-        while True :
+        wfm_path = 'Users/choucurtis987/Desktop/fall2021_honors_project/wfm.txt'
+        wfm_file = open(wfm_path, "w")
+        while True:
             #self.cli_sock.send('???\n'.encode())
             c=''
             data = ''
@@ -21,9 +23,23 @@ class grafit(Frame):
             f = urllib.request.urlopen('http://localhost:5022/?COMMAND=curve?')
             data = f.read().decode()
             print('received '+data)
+            wfm_file.write(data + "\n")
             wfm = [ float(u) for u in data.split(',') ]
+            print(len(wfm))
+
+            # CALLING WFMPRE TO CONVERT WFM TO MS AND VOLTS
+            f2 = urllib.request.urlopen('http://localhost:5022/?COMMAND=wfmpre?')
+            wfmpre = f2.read().decode()
+            print(wfmpre)
+            #wfmpre = '1;8;ASC;RP;MSB;500;"Ch1, AC coupling, 2.0E-2 V/div, 4.0E-5 s/div, 500 points, Average mode";Y;8.0E-7;0;-1.2E-4;"s";8.0E-4;0.0E0;-5.4E1;"V"'
+            t = [ 1.0e6*(float(wfmpre.split(';')[8])*float(i)+float(wfmpre.split(';')[10])) for i in range(0,499) ]
+            volt = [ 1.0e3*(( dl - float(wfmpre.split(';')[14]) )*float(wfmpre.split(';')[12]) - float(wfmpre.split(';')[13])) for dl in wfm ]
+
+            print(f"t: {len(t)}")
+            print(f"volt: {len(volt)}")
+
             # find max and min in first half of wfm
-            peak = np.max(wfm) - np.min(wfm)
+            peak = np.max(volt[:255]) - np.min(volt[:255])
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
             self.xar.append((time.time() - start_time) / 3600.0)
@@ -34,16 +50,34 @@ class grafit(Frame):
                 self.yar.pop(0)
             plt.clf()
 
+            plt.subplot(211)
             plt.plot(self.xar, self.yar,'r-')
             plt.title("Amplitude vs Time")
             plt.ylabel('Counts')
             plt.xlabel('Sample')
 
+            plt.subplot(212)
+            plt.plot(t,volt,'r-')
+            plt.title("Most recent waveform")
+            plt.ylabel("volts")
+            plt.xlabel("time (ms)")
+
+            plt.subplots_adjust(hspace=0.6, wspace=0.6)
             self.plot_widget.grid(row=0, column=0)
 
-            self.fig.canvas.draw_idle() # SEEMS TO BE BUG HERE
+            # plt.plot(volt,t,'r-')
+            # plt.title("Most recent waveform")
+            # plt.ylabel("volts")
+            # plt.xlabel("time (ms)")
+            # self.plot_widget.grid(row=0, column=2)
 
-            #toolbar.update()
+            # WIDGET TO SEE MOST RECENT PEAK
+            p = f"most recent peak: {peak}" # one decimal place is fine
+            # dont use label, use entry (make nicer)
+            # Label(self.window, text=p).grid(row=1, column=0)
+            self.fig.canvas.draw_idle()
+            Label(self.window, text=p).grid(row=1, column=0)
+            # toolbar.update()
 
             # originally 5:
             time.sleep(1.0)
@@ -58,10 +92,12 @@ class grafit(Frame):
         self.yar = []
         self.cli_sock.connect((HOST,PORT))
         self.window = Tk()
+
         self.window.title('Remote data')
 #window.geometry("500x500")
         self.fig = plt.figure(1)
         plt.subplot(111)
+
 #plot1.plot(xar, yar, 'ro-')
         self.fig.text(0.5,0.04,'Time (Hours)',ha ='center',va = 'center')
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)
@@ -72,8 +108,10 @@ class grafit(Frame):
         self.fig.canvas.draw()
 
         self.plotter = threading.Thread(target=self.plotit)
-
+        # self.plotter.setDaemon(True) # thread safe in client but not in server
         self.plotter.start()
+        exit_button = Button(self.window, text="Exit", command=self.window.destroy)
+        exit_button.grid(row=0, column=1)
         self.window.mainloop()
 
 appl = grafit()
