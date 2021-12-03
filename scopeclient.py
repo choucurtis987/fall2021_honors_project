1e578bea87959d0e3717cad55e06fb2ec4a0ac55
Curtis Chou <47016201+choucurtis987@users.noreply.github.com>
2021-12-03 14:01:04 -0800

    updated to be compatible with 16bit sim

--- scopeclient.py
+++ scopeclient.py
@@ -11,8 +11,9 @@ import urllib.request
 class grafit(Frame):
     def plotit(self):
         start_time = time.time()
-        wfm_path = 'Users/choucurtis987/Desktop/fall2021_honors_project/wfm.txt'
-        wfm_file = open(wfm_path, "w")
+        # SAVING TO CSV:
+        # wfm_path = 'Users/choucurtis987/Desktop/fall2021_honors_project/wfm.txt'
+        # wfm_file = open(wfm_path, "w")
         while True:
             #self.cli_sock.send('???\n'.encode())
             c=''
@@ -23,26 +24,32 @@ class grafit(Frame):
             f = urllib.request.urlopen('http://localhost:5022/?COMMAND=curve?')
             data = f.read().decode()
             print('received '+data)
-            wfm_file.write(data + "\n")
+            # SAVING TO CSV:
+            # wfm_file.write(data + "\n")
             wfm = [ float(u) for u in data.split(',') ]
-            print(len(wfm))
+            # print(len(wfm))
 
             # CALLING WFMPRE TO CONVERT WFM TO MS AND VOLTS
             f2 = urllib.request.urlopen('http://localhost:5022/?COMMAND=wfmpre?')
             wfmpre = f2.read().decode()
             print(wfmpre)
             #wfmpre = '1;8;ASC;RP;MSB;500;"Ch1, AC coupling, 2.0E-2 V/div, 4.0E-5 s/div, 500 points, Average mode";Y;8.0E-7;0;-1.2E-4;"s";8.0E-4;0.0E0;-5.4E1;"V"'
-            t = [ 1.0e6*(float(wfmpre.split(';')[8])*float(i)+float(wfmpre.split(';')[10])) for i in range(0,499) ]
-            volt = [ 1.0e3*(( dl - float(wfmpre.split(';')[14]) )*float(wfmpre.split(';')[12]) - float(wfmpre.split(';')[13])) for dl in wfm ]
+            t = [ 1.0e6*(float(wfmpre.split(';')[8])*float(i)+float(wfmpre.split(';')[10])) for i in range(0,len(wfm)) ]
+            volt = [ 1.0e3*(( (dl/256) - float(wfmpre.split(';')[14]) )*float(wfmpre.split(';')[12]) - float(wfmpre.split(';')[13])) for dl in wfm ]
 
-            print(f"t: {len(t)}")
-            print(f"volt: {len(volt)}")
+            # print(f"t: {len(t)}")
+            # print(f"volt: {len(volt)}")
 
-            # find max and min in first half of wfm
-            peak = np.max(volt[:255]) - np.min(volt[:255])
+            # find size of half of volt:
+            half = int( len(volt) / 2 )
+            # take max of (50 to half(volt)) and substract with min of 50 points before half(volt):
+            # we want 50 to half(volt) because max is at times in the beginning
+            # we want 50 points before half(volt) because the change should be so rapid that in the
+            # previous 50 points, there should be a min where the waveform shoots up after
+            peak = np.max(volt[50:half]) - np.min(volt[half-50:half])
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-            self.xar.append((time.time() - start_time) / 3600.0)
+            self.xar.append((time.time() - start_time))
             self.yar.append(peak)
 
             if len(self.xar) > 5000:
@@ -50,20 +57,22 @@ class grafit(Frame):
                 self.yar.pop(0)
             plt.clf()
 
+            # PLOTTTING PEAKS:
             plt.subplot(211)
-            plt.plot(self.xar, self.yar,'r-')
+            plt.plot(self.xar, self.yar,'ro-')
             plt.title("Amplitude vs Time")
             plt.ylabel('Counts')
             plt.xlabel('Sample')
 
+            # PLOTTING WAVEFORM:
             plt.subplot(212)
-            plt.plot(t,volt,'r-')
+            plt.plot(t,volt,'ro-', color='green')
             plt.title("Most recent waveform")
-            plt.ylabel("volts")
-            plt.xlabel("time (ms)")
+            plt.ylabel("Volts")
+            plt.xlabel(u"Time (\u03bcs)")
 
             plt.subplots_adjust(hspace=0.6, wspace=0.6)
-            self.plot_widget.grid(row=0, column=0)
+            self.plot_widget.grid(row=0, column=0, rowspan=2)
 
             # plt.plot(volt,t,'r-')
             # plt.title("Most recent waveform")
@@ -72,11 +81,13 @@ class grafit(Frame):
             # self.plot_widget.grid(row=0, column=2)
 
             # WIDGET TO SEE MOST RECENT PEAK
-            p = f"most recent peak: {peak}" # one decimal place is fine
-            # dont use label, use entry (make nicer)
-            # Label(self.window, text=p).grid(row=1, column=0)
+            T = Text(self.window, height = 5, width = 5)
+            peak = round(peak, 1)
+            T.insert(END, peak)
+            T.grid(row=0, column=1)
+
+
             self.fig.canvas.draw_idle()
-            Label(self.window, text=p).grid(row=1, column=0)
             # toolbar.update()
 
             # originally 5:
@@ -96,10 +107,11 @@ class grafit(Frame):
         self.window.title('Remote data')
 #window.geometry("500x500")
         self.fig = plt.figure(1)
-        plt.subplot(111)
+        self.fig.text(0.5,0.04,'LOADING...',ha ='center',va = 'center')
+        # plt.subplot(111)
 
 #plot1.plot(xar, yar, 'ro-')
-        self.fig.text(0.5,0.04,'Time (Hours)',ha ='center',va = 'center')
+        # self.fig.text(0.5,0.04,'Time (Hours)',ha ='center',va = 'center')
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)
         self.plot_widget = self.canvas.get_tk_widget()
         self.plot_widget.grid(row=0, column=0)
@@ -111,7 +123,7 @@ class grafit(Frame):
         # self.plotter.setDaemon(True) # thread safe in client but not in server
         self.plotter.start()
         exit_button = Button(self.window, text="Exit", command=self.window.destroy)
-        exit_button.grid(row=0, column=1)
+        exit_button.grid(row=1, column=1)
         self.window.mainloop()
 
 appl = grafit()
